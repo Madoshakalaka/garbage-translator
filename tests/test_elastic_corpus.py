9462a5abb70ecb141716581e536b16eadcab2809
matt <syan4@ualberta.ca>
2019-08-28 00:51:33 -0600

    done

--- tests/test_elastic_corpus.py
+++ tests/test_elastic_corpus.py
@@ -7,7 +7,7 @@ from garbage_translator.main import ElasticCorpus
 @pytest.mark.parametrize("extras", (["d"], ["e"], ["f"], ["a"], ["a", "e", "f"]))
 def test_elastic_corpus(corpus_words: List[str], extras: List[str]):
     corpus_words.sort()
-    corpus = ElasticCorpus(corpus_words, False, {})
+    corpus = ElasticCorpus(corpus_words, False, {}, corpus_words)
     original_corpus_words = corpus_words.copy()
     with corpus.extra_words_inserted(set(extras)) as bloated:
         assert bloated._sorted_words == sorted(bloated._sorted_words)
